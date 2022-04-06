d5ae8eefefe80df65e661c8c5a304db51c843207
Khushal Tiwary <65728465+khushaltiwary@users.noreply.github.com>
2022-04-06 16:08:30 +0530

    drawer layout -> simple list type menu done

--- KnowledgeSpaceApk/app/src/main/java/com/example/knowledgespaceapk/homeSc.java
+++ KnowledgeSpaceApk/app/src/main/java/com/example/knowledgespaceapk/homeSc.java
@@ -1,14 +1,73 @@
 package com.example.knowledgespaceapk;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class homeSc extends AppCompatActivity {
-
+NavigationView navigationView;
+ActionBarDrawerToggle toggle;
+DrawerLayout drawerLayout;
+Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_sc);
-    }
-}
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        navigationView = findViewById(R.id.navView);
+        drawerLayout = findViewById(R.id.drawer);
+
+        toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.academic:
+                        Toast.makeText(homeSc.this, "Academic clicked", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    case R.id.exam:
+                        Toast.makeText(homeSc.this, "Exams clicked", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    case R.id.avatar:
+                        Toast.makeText(homeSc.this, "Avatar clicked", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    case R.id.posts:
+                        Toast.makeText(homeSc.this, "Posts clicked", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    case R.id.settings:
+                        Toast.makeText(homeSc.this, "Settings clicked", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    case R.id.termsNconditions:
+                        Toast.makeText(homeSc.this, "TnC clicked", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    case R.id.signout:
+                        Toast.makeText(homeSc.this, "SignOut clicked", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                }
+                return true;
+            }
+        });
+    }//End OnCreate
+}//End Main
