94421bdde85705eb0f7ec7b109a9d0e8da3af5f7
Khushal Tiwary <65728465+khushaltiwary@users.noreply.github.com>
2022-04-11 13:36:52 +0530

    bottom nav + drawer nav -> working all fine

--- KnowledgeSpaceApk/app/src/main/java/com/example/knowledgespaceapk/homeSc.java
+++ KnowledgeSpaceApk/app/src/main/java/com/example/knowledgespaceapk/homeSc.java
@@ -1,6 +1,6 @@
 package com.example.knowledgespaceapk;
 
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -9,28 +9,35 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+
 import android.os.Bundle;
-import android.view.MenuItem;
+
 
 import com.example.knowledgespaceapk.databinding.ActivityHomeScBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationView;
+
 
 public class homeSc extends AppCompatActivity {
-
-    ActivityHomeScBinding homeScBinding;
+    private DrawerLayout drawerLayout;
+    private BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        homeScBinding = ActivityHomeScBinding.inflate(getLayoutInflater());
-        setContentView(homeScBinding.getRoot());
-        replaceFrag(new HomeFragment());
+        setContentView(R.layout.activity_home_sc);
 
-        homeScBinding.bottomNavigationView.setOnItemSelectedListener(item -> {
+        Toolbar toolbar = findViewById(R.id.drawerNavigationToolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawerNavigationDrawerLayout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,
+                            toolbar,R.string.open,R.string.close);
+       drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+        replaceFrag(new HomeFragment());
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
 
             switch (item.getItemId()){
                 case R.id.home : replaceFrag(new HomeFragment()); break;
@@ -41,6 +48,15 @@ public class homeSc extends AppCompatActivity {
         });
 
     }//End OnCreate
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+    }
 
     private  void replaceFrag(Fragment fragment){
         FragmentManager fragmentManager = getSupportFragmentManager();
