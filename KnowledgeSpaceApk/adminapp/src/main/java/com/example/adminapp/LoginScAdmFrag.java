769f106a69b5e43fd6fbb0eb437b5556e94aebeb
Khushal Tiwary <65728465+khushaltiwary@users.noreply.github.com>
2022-10-20 11:32:21 +0530

    Updated admin app build gradle with newer version availables

---
+++ KnowledgeSpaceApk/adminapp/src/main/java/com/example/adminapp/LoginScAdmFrag.java
@@ -0,0 +1,31 @@
+package com.example.adminapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class LoginScAdmFrag extends Fragment {
+
+    EditText emailTvLoginScAdm,passTvLoginScAdm;
+    Button loginBtnLogicScAdm;
+
+
+    public LoginScAdmFrag() {
+        // Required empty public constructor
+    }
+
+    
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login_sc_adm, container, false);
+    }
+}
