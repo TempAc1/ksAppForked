11ac72590573c8540ac34bffce4f8752a3b1b997
Khushal Tiwary <65728465+khushaltiwary@users.noreply.github.com>
2022-10-20 15:26:38 +0530

    Made LoginScAdmFrag.java -> app working fine!

--- KnowledgeSpaceApk/adminapp/src/main/java/com/example/adminapp/LoginScAdmFrag.java
+++ KnowledgeSpaceApk/adminapp/src/main/java/com/example/adminapp/LoginScAdmFrag.java
@@ -1,21 +1,33 @@
 package com.example.adminapp;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class LoginScAdmFrag extends Fragment {
 
     EditText emailTvLoginScAdm,passTvLoginScAdm;
     Button loginBtnLogicScAdm;
-
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private String email,pass;
 
     public LoginScAdmFrag() {
         // Required empty public constructor
@@ -26,6 +38,58 @@ public class LoginScAdmFrag extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login_sc_adm, container, false);
+        View view = inflater.inflate(R.layout.fragment_login_sc_adm,container,false);
+        loginBtnLogicScAdm = view.findViewById(R.id.loginBtnLogicScAdm);
+        emailTvLoginScAdm = view.findViewById(R.id.emailTvLoginScAdm);
+        passTvLoginScAdm = view.findViewById(R.id.passTvLoginScAdm);
+        loginBtnLogicScAdm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gAuth();
+            }
+        });
+        return view;
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user!=null){
+            startActivity(new Intent(getActivity(),HomeScAdm.class));
+        }else{
+            gAuth();
+        }
+    }
+
+    private void gAuth() {
+        if(emailTvLoginScAdm.getText()!=null && passTvLoginScAdm.getText()!=null){
+            email = emailTvLoginScAdm.getText().toString().trim();
+            pass = passTvLoginScAdm.getText().toString().trim();
+            if(!email.matches("") && !pass.matches("")){
+                mAuth.signInWithEmailAndPassword(email,pass)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(getActivity(), "Login Successfully", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(getActivity(),HomeScAdm.class));
+                                }else{
+                                    Toast.makeText(getActivity(), "Login Failure", Toast.LENGTH_SHORT).show();
+                                    Log.i("Else part",task.getException().getMessage());
+                                    Log.i("Else part",pass);
+                                }
+                            }
+                        });
+            }else{
+                Drawable customErrorDrawable = getResources().getDrawable(R.drawable.ic_baseline_error_24);
+                customErrorDrawable.setBounds(0,0,customErrorDrawable.getIntrinsicWidth(),
+                            customErrorDrawable.getIntrinsicHeight());
+                emailTvLoginScAdm.setError("Field cannot be empty");
+                passTvLoginScAdm.setError("Field cannot be empty");
+            }
+        }else{
+            Toast.makeText(getActivity(), "Field cannot be left empty", Toast.LENGTH_SHORT).show();
+        }
+    }//End googleAuth
 }
--- KnowledgeSpaceApk/adminapp/src/main/java/com/example/adminapp/MainActivity.java
+++ KnowledgeSpaceApk/adminapp/src/main/java/com/example/adminapp/MainActivity.java
@@ -4,19 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.adminapp.AttendanceAct.AttendanceAct;
 import com.example.adminapp.SettingsAct.settingAct;
 import com.example.adminapp.contactAct.ContactAct;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView imgviewMainactAdm;
+    TextView tVMainAct2Adm,textViewMainActAdm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent i = new Intent(getApplicationContext(),HomeScAdm.class);
-        startActivity(i);
+        imgviewMainactAdm = findViewById(R.id.imgviewMainactAdm);
+        tVMainAct2Adm = findViewById(R.id.tVMainAct2Adm);
+        textViewMainActAdm = findViewById(R.id.textViewMainActAdm);
+
+       new Handler().postDelayed(new Runnable() {
+           @Override
+           public void run() {
+               hideAll();
+               getSupportFragmentManager().beginTransaction()
+                       .add(R.id.containerFragMainActAdm,new LoginScAdmFrag()).commit();
+           }
+       },1500);
+    }
+
+    private void hideAll() {
+        imgviewMainactAdm.setVisibility(View.GONE);
+        textViewMainActAdm.setVisibility(View.GONE);
+        textViewMainActAdm.setVisibility(View.GONE);
     }
 }
