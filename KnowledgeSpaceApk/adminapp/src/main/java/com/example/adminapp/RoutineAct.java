434ad9051aa63dae35331f64723b705d097ce4ff
Khushal Tiwary <65728465+khushaltiwary@users.noreply.github.com>
2022-10-28 17:49:44 +0530

    Made RoutineAct.java take img from gallery and upload in Ui only-> All app working fine!

--- KnowledgeSpaceApk/adminapp/src/main/java/com/example/adminapp/RoutineAct.java
+++ KnowledgeSpaceApk/adminapp/src/main/java/com/example/adminapp/RoutineAct.java
@@ -1,15 +1,29 @@
 package com.example.adminapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class RoutineAct extends AppCompatActivity {
 
     private Button routineBtn;
+    private ImageView routineImgV;
+    private int PICKFILE_RESULT_CODE=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +31,37 @@ public class RoutineAct extends AppCompatActivity {
         setContentView(R.layout.activity_routine);
 
         routineBtn = findViewById(R.id.uploadRoutineBtn);
+        routineImgV = findViewById(R.id.routineImgVRoutineAct);
         uploadRoutine();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICKFILE_RESULT_CODE && data !=null && data.getData() != null) {
+            Uri uri = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                // Log.d(TAG, String.valueOf(bitmap));
+
+                routineImgV.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                Toast.makeText(this, "Internal Error", Toast.LENGTH_SHORT).show();;
+            }
+        }
     }
 
     private void uploadRoutine() {
         routineBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(RoutineAct.this, "Uploading...", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICKFILE_RESULT_CODE);
+
             }
         });
     }
+
 }
