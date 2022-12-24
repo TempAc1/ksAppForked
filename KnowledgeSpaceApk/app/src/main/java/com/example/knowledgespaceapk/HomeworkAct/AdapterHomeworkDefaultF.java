15e963f359fb791c393c12293930d31cf5e40502
Khushal Tiwary <65728465+khushaltiwary@users.noreply.github.com>
2022-12-24 13:22:43 +0530

    Made HomeworkDetailsF-> Need to change in accordance -> App working Fine

--- KnowledgeSpaceApk/app/src/main/java/com/example/knowledgespaceapk/HomeworkAct/AdapterHomeworkDefaultF.java
+++ KnowledgeSpaceApk/app/src/main/java/com/example/knowledgespaceapk/HomeworkAct/AdapterHomeworkDefaultF.java
@@ -6,6 +6,10 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.knowledgespaceapk.R;
@@ -42,9 +46,23 @@ public class AdapterHomeworkDefaultF extends RecyclerView.Adapter<AdapterHomewor
 
     public class myviewholder extends RecyclerView.ViewHolder {
         TextView hwTitleHomeworkDefaultFrag;
+        CardView cardVAndroidXSingleRdScholarship;
         public myviewholder(@NonNull View itemView) {
             super(itemView);
             hwTitleHomeworkDefaultFrag = itemView.findViewById(R.id.homeTitleTvSingleRdHomeworkDefaultFrag);
+
+            cardVAndroidXSingleRdScholarship = itemView.findViewById(R.id.cardVAndroidXSingleRdScholarship);
+            cardVAndroidXSingleRdScholarship.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    AppCompatActivity activity = (AppCompatActivity) v.getContext();
+                    FragmentManager fragmentManager = activity.getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.frameLayHomeworkMainAct,new HomeworkDetailsF());
+                    fragmentTransaction.addToBackStack(String.valueOf(new HomeworkDetailsF()));
+                    fragmentTransaction.commit();
+                }
+            });
         }
     }
 }
