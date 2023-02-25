04e3869e38349bc46e9f5e769d5c27c11b0138cc
Khushal Tiwary <65728465+khushaltiwary@users.noreply.github.com>
2023-02-25 14:02:24 +0530

    Deleted fragment_hall_of_frame_frag_adm.xml n CustomAdapter LAyout

--- KnowledgeSpaceApk/adminapp/src/main/java/com/example/adminapp/AlumniActAdm/AlumniFragHomeSc.java
+++ KnowledgeSpaceApk/adminapp/src/main/java/com/example/adminapp/AlumniActAdm/AlumniFragHomeSc.java
@@ -14,7 +14,6 @@ import android.widget.FrameLayout;
 import android.widget.ScrollView;
 
 import com.example.adminapp.AlumniActAdm.AlumniDirectory.AlumniDirectoryMainSc.AlumniDirectoryMainScFAdm;
-import com.example.adminapp.AlumniActAdm.HallOfFrame.HallOfFrameFragAdm;
 import com.example.adminapp.AlumniActAdm.Opportunity.AlumniOpportunityFragAdm;
 import com.example.adminapp.R;
 import com.google.android.material.card.MaterialCardView;
@@ -79,14 +78,14 @@ public class AlumniFragHomeSc extends Fragment {
         HallOfFrameMaterialCardVAlumniFragMainSc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AppCompatActivity activity = (AppCompatActivity) view.getContext();
-                FrameLayout fl = activity.findViewById(R.id.frameLayAlumniFragHomeSc);
-                fl.removeAllViews();
-                Fragment fragment = new HallOfFrameFragAdm();
-                FragmentManager manager = activity.getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction = manager.beginTransaction();
-                fragmentTransaction.replace(R.id.frameLayAlumniFragHomeSc,fragment).commit();
-                fragmentTransaction.addToBackStack(String.valueOf(R.layout.fragment_alumni_frag_home_sc));
+//                AppCompatActivity activity = (AppCompatActivity) view.getContext();
+//                FrameLayout fl = activity.findViewById(R.id.frameLayAlumniFragHomeSc);
+//                fl.removeAllViews();
+//                Fragment fragment = new HallOfFrameFragAdm();
+//                FragmentManager manager = activity.getSupportFragmentManager();
+//                FragmentTransaction fragmentTransaction = manager.beginTransaction();
+//                fragmentTransaction.replace(R.id.frameLayAlumniFragHomeSc,fragment).commit();
+//                fragmentTransaction.addToBackStack(String.valueOf(R.layout.fragment_alumni_frag_home_sc));
 
             }
         });
