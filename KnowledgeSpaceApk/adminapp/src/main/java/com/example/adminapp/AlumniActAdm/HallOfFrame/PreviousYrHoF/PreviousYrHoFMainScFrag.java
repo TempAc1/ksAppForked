ca48b44e833eafb01a532c4d6e703fdb9126a42e
Khushal Tiwary <65728465+khushaltiwary@users.noreply.github.com>
2023-03-05 05:07:46 +0530

    Made SelectYr Spinner in SelectYrPrevYrAlumniHof -> App working fine

--- KnowledgeSpaceApk/adminapp/src/main/java/com/example/adminapp/AlumniActAdm/HallOfFrame/PreviousYrHoF/PreviousYrHoFMainScFrag.java
+++ KnowledgeSpaceApk/adminapp/src/main/java/com/example/adminapp/AlumniActAdm/HallOfFrame/PreviousYrHoF/PreviousYrHoFMainScFrag.java
@@ -16,8 +16,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.adminapp.AlumniActAdm.HallOfFrame.PreviousYrHoF.RecyclerVMainSc.AdapterRecVMainScPrevYrHoFAdm;
 import com.example.adminapp.AlumniActAdm.HallOfFrame.PreviousYrHoF.RecyclerVMainSc.DataModelRecVMainScPrevYrHoF;
@@ -26,13 +30,17 @@ import com.example.adminapp.R;
 import java.util.ArrayList;
 
 
-public class PreviousYrHoFMainScFrag extends Fragment {
+public class PreviousYrHoFMainScFrag extends Fragment implements AdapterView.OnItemSelectedListener {
 
     private RecyclerView recyclerVPrevYrHoFMainScAdm;
     private TextView selectYrPrevYrHoFMainScFAdm;
     private Button searchBtnPrevYrHoFMainScFragAdm;
     private ArrayList<DataModelRecVMainScPrevYrHoF> dataHolder;
+    private Spinner spinnerSelectYearPrevYrHoFMainScFAdm;
+    private ArrayList<DataModelForSpinnerPrevYrOpts> dataHolderSpinner;
 
+
+    private static final String[] years = {"20XX", "2019", "2020","2021","2022","2023"};
 
     public PreviousYrHoFMainScFrag() {
         // Required empty public constructor
@@ -50,6 +58,19 @@ public class PreviousYrHoFMainScFrag extends Fragment {
 //        selectYrPrevYrHoFMainScFAdm = view.findViewById(R.id.selectYrPrevYrHoFMainScFAdm);
         searchBtnPrevYrHoFMainScFragAdm = view.findViewById(R.id.searchBtnPrevYrHoFMainScFragAdm);
 
+        spinnerSelectYearPrevYrHoFMainScFAdm = view.findViewById(R.id.spinnerSelectYearPrevYrHoFMainScFAdm);
+
+        //Todo koi method h iske liye?
+        dataHolderSpinner = new ArrayList<>();
+        addSpinnerData();
+
+
+        ArrayAdapter<String>arrayAdapter = new ArrayAdapter<String>(getContext(),
+                android.R.layout.simple_spinner_dropdown_item,years);
+        arrayAdapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+        spinnerSelectYearPrevYrHoFMainScFAdm.setAdapter(arrayAdapter);
+        spinnerSelectYearPrevYrHoFMainScFAdm.setOnItemSelectedListener(this);
+
         GridLayoutManager gridLayoutManager = new GridLayoutManager(view.getContext(),2);
         recyclerVPrevYrHoFMainScAdm.setLayoutManager(gridLayoutManager);
         DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(getContext(),DividerItemDecoration.VERTICAL);
@@ -59,6 +80,22 @@ public class PreviousYrHoFMainScFrag extends Fragment {
         recyclerVPrevYrHoFMainScAdm.setAdapter(new AdapterRecVMainScPrevYrHoFAdm(dataHolder));
 
         return view;
+    }
+
+    private void addSpinnerData() {
+        DataModelForSpinnerPrevYrOpts obj = new DataModelForSpinnerPrevYrOpts(2019);
+        dataHolderSpinner.add(obj);
+        dataHolderSpinner.add(obj);
+        DataModelForSpinnerPrevYrOpts obj2 = new DataModelForSpinnerPrevYrOpts(2020);
+        dataHolderSpinner.add(obj2);
+        DataModelForSpinnerPrevYrOpts obj3 = new DataModelForSpinnerPrevYrOpts(2022);
+        dataHolderSpinner.add(obj3);
+        DataModelForSpinnerPrevYrOpts obj4 = new DataModelForSpinnerPrevYrOpts(2023);
+        dataHolderSpinner.add(obj4);
+        DataModelForSpinnerPrevYrOpts obj5 = new DataModelForSpinnerPrevYrOpts(2024);
+        dataHolderSpinner.add(obj5);
+        DataModelForSpinnerPrevYrOpts obj6 = new DataModelForSpinnerPrevYrOpts(2025);
+        dataHolderSpinner.add(obj6);
     }
 
     //Todo add backend linked profile link
@@ -81,4 +118,34 @@ public class PreviousYrHoFMainScFrag extends Fragment {
     }
 
 
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch (position){
+            case 0:
+                //Do nothing
+                break;
+            case 1:
+                Toast.makeText(getContext(), "1", Toast.LENGTH_SHORT).show();
+                break;
+            case 2:
+                Toast.makeText(getContext(), "2", Toast.LENGTH_SHORT).show();
+                break;
+            case 3:
+                Toast.makeText(getContext(), "3", Toast.LENGTH_SHORT).show();
+                break;
+            case 4:
+                Toast.makeText(getContext(), "4", Toast.LENGTH_SHORT).show();
+                break;
+            case 5:
+                Toast.makeText(getContext(), "5", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
